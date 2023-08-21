265e3b7e02a0d2453d0548eca21442b49e60d23f
Arun <arunkarthick0810@gmail.com>
2023-08-21 06:53:58 +0530

    netflix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const connection = process.env.CONNECTION_URL;
 import userRoutes from "./routes/UserRoutes.js";
 import cors from "cors";
 
-app.get("/mes", (req, res) => {
+app.get("/", (req, res) => {
   res.json("welcome");
 });
 
